0ddb962ff1925062b5c5a60651ad6caab705c89e
Ronnald R Machado <rrmachado@id.uff.br>
2018-07-16 16:36:39 -0300

    Build trait and testing

---
+++ medley.php
@@ -0,0 +1,37 @@
+<?php
+
+trait medley{
+	private function mecanica_courses(){
+		$courses = array('medley_mecanica'=>array('ENG1700','ENG1701','ENG1702'));
+		return $courses;
+		/*array('ENG1031','ENG1032','ENG1700','ENG1701','ENG1702','ENG1703','ENG1704','ENG1705'
+        							,'ENG1707','ENG1708','ENG1709','ENG1710','ENG1712','ENG1713','ENG1714','ENG1715',
+        							'ENG1716','ENG1717','ENG1718','ENG1719','ENG1720','ENG1721','ENG1784');*/
+	}
+
+	private function adm_courses(){
+		$courses = array('medley_adm'=>array('ADM1000','ADM1001'));
+		return $courses;
+
+	}
+
+	public function get_medley_courses(){
+		$mecanica = $this->mecanica_courses();
+		$adm = $this->adm_courses();
+
+		return array_merge($mecanica,$adm);
+
+	}
+
+	public function name_medley_courses(){
+		$courses = $this->get_medley_courses();
+		$courses = array_keys($courses);
+		return $courses;
+
+	}
+
+	public function teste_trait(){
+		return 'testeasdasdasd';
+	}
+
+}
